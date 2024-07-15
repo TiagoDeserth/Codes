c4a51412aedecaa336978354add51f518b8e5c01
Tiago Carlos Batista <tiago_net_3@hotmail.com>
2024-07-15 10:29:51 -0300

    First commit

---
+++ teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int result = a + b;
+    //printf("Hello, World!\n");
+    printf("Result: %d \n", result);
+    return 0;
+}
+
